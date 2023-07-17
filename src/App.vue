d4eff192e8acbf908f42ae9f85063697e82d3eef
Johnnick F. Landim <78829381+johnnickjf@users.noreply.github.com>
2023-07-17 12:28:58 -0300

    create: Project

---
+++ src/App.vue
@@ -0,0 +1,211 @@
+<template>
+  <v-app theme="dark">
+    <v-app-bar>
+      <v-app-bar-title class="text-h5">DASHBOARD</v-app-bar-title>
+
+      <template #append>
+        <v-btn icon class="mr-2">
+          <v-badge dot color="info">
+            <v-icon icon="mdi-bell-outline"></v-icon>
+          </v-badge>
+        </v-btn>
+      </template>
+      <template #prepend>
+        <v-avatar>
+          <v-img src="https://images.pexels.com/photos/426976/pexels-photo-426976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></v-img>
+        </v-avatar>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <h1 class="text-center ma-5">Sorteios</h1>
+        <v-row>
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg" class="align-end text-white">
+                <v-card-title class="text-h">Sorteio 1</v-card-title>
+              </v-img>
+              <v-card-item class="ma-0">
+                <v-card-title>Honda Civic 2019</v-card-title>
+
+                <v-card-subtitle>
+                  <span class="me-1">Turbo</span>
+
+                  <v-icon
+                    color="green"
+                    icon="mdi-cash"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+              </v-card-item>
+              <v-card-title class="ma-0">R$ 19,90</v-card-title>
+              <v-card-text>
+                <div>Descrição do sorteio</div>
+                <div>Participantes: 10</div>
+                <div>Sorteio em: 10/10/2021</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" variant="tonal">Participar</v-btn>
+                <v-btn color="primary" variant="tonal">Ver Mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg" class="align-end text-white">
+                <v-card-title class="text-h">Sorteio 1</v-card-title>
+              </v-img>
+              <v-card-item class="ma-0">
+                <v-card-title>Honda Civic 2019</v-card-title>
+
+                <v-card-subtitle>
+                  <span class="me-1">Turbo</span>
+
+                  <v-icon
+                    color="green"
+                    icon="mdi-cash"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+              </v-card-item>
+              <v-card-title class="ma-0">R$ 19,90</v-card-title>
+              <v-card-text>
+                <div>Descrição do sorteio</div>
+                <div>Participantes: 10</div>
+                <div>Sorteio em: 10/10/2021</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" variant="tonal">Participar</v-btn>
+                <v-btn color="primary" variant="tonal">Ver Mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg" class="align-end text-white">
+                <v-card-title class="text-h">Sorteio 1</v-card-title>
+              </v-img>
+              <v-card-item class="ma-0">
+                <v-card-title>Honda Civic 2019</v-card-title>
+
+                <v-card-subtitle>
+                  <span class="me-1">Turbo</span>
+
+                  <v-icon
+                    color="green"
+                    icon="mdi-cash"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+              </v-card-item>
+              <v-card-title class="ma-0">R$ 19,90</v-card-title>
+              <v-card-text>
+                <div>Descrição do sorteio</div>
+                <div>Participantes: 10</div>
+                <div>Sorteio em: 10/10/2021</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" variant="tonal">Participar</v-btn>
+                <v-btn color="primary" variant="tonal">Ver Mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg" class="align-end text-white">
+                <v-card-title class="text-h">Sorteio 1</v-card-title>
+              </v-img>
+              <v-card-item class="ma-0">
+                <v-card-title>Honda Civic 2019</v-card-title>
+
+                <v-card-subtitle>
+                  <span class="me-1">Turbo</span>
+
+                  <v-icon
+                    color="green"
+                    icon="mdi-cash"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+              </v-card-item>
+              <v-card-title class="ma-0">R$ 19,90</v-card-title>
+              <v-card-text>
+                <div>Descrição do sorteio</div>
+                <div>Participantes: 10</div>
+                <div>Sorteio em: 10/10/2021</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" variant="tonal">Participar</v-btn>
+                <v-btn color="primary" variant="tonal">Ver Mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg" class="align-end text-white">
+                <v-card-title class="text-h">Sorteio 1</v-card-title>
+              </v-img>
+              <v-card-item class="ma-0">
+                <v-card-title>Honda Civic 2019</v-card-title>
+
+                <v-card-subtitle>
+                  <span class="me-1">Turbo</span>
+
+                  <v-icon
+                    color="green"
+                    icon="mdi-cash"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+              </v-card-item>
+              <v-card-title class="ma-0">R$ 19,90</v-card-title>
+              <v-card-text>
+                <div>Descrição do sorteio</div>
+                <div>Participantes: 10</div>
+                <div>Sorteio em: 10/10/2021</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" variant="tonal">Participar</v-btn>
+                <v-btn color="primary" variant="tonal">Ver Mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img src="https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg" class="align-end text-white">
+                <v-card-title class="text-h">Sorteio 1</v-card-title>
+              </v-img>
+              <v-card-item class="ma-0">
+                <v-card-title>Honda Civic 2019</v-card-title>
+
+                <v-card-subtitle>
+                  <span class="me-1">Turbo</span>
+
+                  <v-icon
+                    color="green"
+                    icon="mdi-cash"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+              </v-card-item>
+              <v-card-title class="ma-0">R$ 19,90</v-card-title>
+              <v-card-text>
+                <div>Descrição do sorteio</div>
+                <div>Participantes: 10</div>
+                <div>Sorteio em: 10/10/2021</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" variant="tonal">Participar</v-btn>
+                <v-btn color="primary" variant="tonal">Ver Mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
